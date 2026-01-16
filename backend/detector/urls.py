a45c2e4e4ee1e6bd78a9c0648088d844bddc3bff
Shanny254 <sharlynenyaboke@gmail.com>
2026-01-16 22:09:07 +0300

    Detector Urls

--- backend/detector/urls.py
+++ backend/detector/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import predict_fraud
+from .views import predict_fraud, home
 
 urlpatterns = [
+    path("", home),
     path("predict/", predict_fraud),
 ]
