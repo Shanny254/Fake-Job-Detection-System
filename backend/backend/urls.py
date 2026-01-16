7bedaf4a75adc095e009aa2c810b9c8ba34d0d6f
Shanny254 <sharlynenyaboke@gmail.com>
2026-01-16 22:08:12 +0300

    Backend Urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("detector.urls")),
     path("admin/", admin.site.urls),
-    path("api/", include("detector.urls")),
 ]
 
